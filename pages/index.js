dd52704ebb1c52f452d7f099675ac95d60fc52e0
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2019-03-05 21:11:00 +0700

    Link: Learn about how to move to other page from client-side

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-const Index = () => {
-    return <React.Fragment>
+import Link from 'next/link'
+const Index = () => (
+    <React.Fragment>
+        <Link href='/about' style={{fontSize:204}}>
+            <a>About Us</a>
+        </Link>
         <p>Hello Next.js</p>
     </React.Fragment>
-}
+)
 
 export default Index
